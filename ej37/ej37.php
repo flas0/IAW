d0b2cdf30c4019b7aadfe31b12fe7283af2f2967
flas0 <mckenrrow@gmail.com>
2020-12-05 15:36:33 +0100

    .

---
+++ ej37/ej37.php
@@ -0,0 +1,23 @@
+<?php
+
+print "INSERCIÓN DE LA VIVIENDA </br></br>";
+
+print "Los datos introducidos son: </br>";
+
+print "El tipo de vivienda elegido es  $_GET[tipov].</br>"; 
+
+print "La zona seleccionada es $_GET[tipoz].</br>"; 
+
+print "La dirección introducida es $_GET[direccion].</br>"; 
+
+print "La casa cuenta con $_GET[nhab] habitaciones.</br>"; 
+
+print "Su precio es $_GET[precio].</br>"; 
+
+print "El tamaño es de  $_GET[tamaño] m2.</br>"; 
+
+print "Extras: $_GET[extras].</br>"; 
+
+print "Observaciones: $_GET[texto].</br>"; 
+
+?>
